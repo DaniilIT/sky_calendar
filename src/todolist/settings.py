f9e70419a8f19b38e3eaf1b43c894d8ca2176524
Manuilov Daniil <manuilovdaniil@yandex.ru>
2023-05-27 10:01:45 +0300

    set ci/cd

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -21,8 +21,11 @@ env = environ.Env(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE_PATH = BASE_DIR.parent / '.env'
 
-environ.Env.read_env(BASE_DIR / '.env')
+if ENV_FILE_PATH.exists() and ENV_FILE_PATH.is_file():
+    # в docker контейнере используются переменные окружения
+    environ.Env.read_env(ENV_FILE_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
