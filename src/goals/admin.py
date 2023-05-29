7de4a396757e375972495df7024697410fee2615
Manuilov Daniil <manuilovdaniil@yandex.ru>
2023-05-29 23:52:59 +0300

    feature add category admin

--- src/goals/admin.py
+++ src/goals/admin.py
@@ -1,3 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from goals.models import GoalCategory
+
+
+@admin.register(GoalCategory)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'is_deleted')
+    list_display_links = ('title',)
+    list_editable = ('is_deleted',)
+    search_fields = ('title',)
+    list_filter = ('is_deleted',)
+
+    raw_id_fields = ('user',)
+    readonly_fields = ('created', 'updated')
